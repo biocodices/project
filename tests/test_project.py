96452cfc8f0fd735d53e76a8d41fabef0c15eb3c
Juan Manuel Berros <juanmaberros@gmail.com>
2016-11-24 18:30:04 -0300

    pytest fixes

--- tests/test_project.py
+++ tests/test_project.py
@@ -11,12 +11,11 @@ TEST_DIR = dirname(realpath(__file__))
 test_project = Project(join(TEST_DIR, 'test_project'))
 
 
-def test_initialization():
-    pj = Project(join(TEST_DIR, 'unexistent_dir'))
+def test_initialization(tmpdir):
+    pj = Project(str(tmpdir))
 
     for directory in [pj.data_dir, pj.results_dir, pj.dir]:
         assert isdir(directory)
-        rmdir(directory)  # Cleanup
 
 def test_subdir_lookup():
     pj = test_project
@@ -33,26 +32,25 @@ def test_subdir_lookup():
     csvs = pj.data_files(regex=r'data_.+\.(csv|tsv)')
     assert 'data_file.csv' in [basename(f) for f in csvs]
 
-def test_dump_df():
+def test_read_csv():
+    pj = test_project
+    df = pj.read_csv('data_file.csv', subdir='data')
+    assert all(type(item) is dict for item in df['dicts'])
+    assert all(type(item) is list for item in df['lists'])
+
+def test_dump_df(tmpdir):
     df = pd.DataFrame({
             'a': [1, 2, 3],
             'b': [['1'], ['2'], ['3']],
             'c': [{'a': 1}, {'b': 2}, {'c': 3}]
         })
 
-    pj = test_project
+    pj = Project(str(tmpdir))
     fn = pj.dump_df(df, 'test_dump')
 
     assert isfile(fn)
-    remove(fn)
 
     fn = pj.dump_df(df, 'test_dump.tsv')
     assert isfile(fn)
     assert all(pd.read_table(fn).columns == ['a', 'b', 'c'])
-    remove(fn)  # Cleanup
 
-def test_read_csv():
-    pj = test_project
-    df = pj.read_csv('data_file.csv', subdir='data')
-    assert all(type(item) is dict for item in df['dicts'])
-    assert all(type(item) is list for item in df['lists'])
